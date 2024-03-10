f0cb2de968cff1e2decf124cea39e34d54800b11
Jan Kielmann <jan.kielmann@sap.com>
2024-03-10 18:54:03 +0100

    Make app configurable via environment variables

--- recorder_service/config.py
+++ recorder_service/config.py
@@ -5,8 +5,14 @@ from recorder_service.core.recorder_service import RecorderService
 from recorder_service.video import LocalVideoSource
 from recorder_service.storage import LocalFilesystemStorage
 
-print("TEST ENV: ", os.getenv("TEST"))
+# Load configuration from environment variables
+local_video_path = Path(os.getenv("LOCAL_VIDEO", default="data/test_video.mp4"))
+if not local_video_path.exists():
+    raise FileNotFoundError(f"Local video file {local_video_path} does not exist. Ensure environment variable LOCAL_VIDEO is set correctly.")
+storage_dir = Path(os.getenv("STORAGE_DIR", default="data/storage"))
+num_frames_to_capture = int(os.getenv("NUM_FRAMES_TO_CAPTURE", default=5))
 
-video_source = LocalVideoSource(Path("data/test_video.mp4"))
-storage_service = LocalFilesystemStorage(Path("data/storage"))
-recorder_service = RecorderService(3, video_source, storage_service)
+# Set up the recorder service
+video_source = LocalVideoSource(local_video_path)
+storage_service = LocalFilesystemStorage(storage_dir)
+recorder_service = RecorderService(num_frames_to_capture, video_source, storage_service)
